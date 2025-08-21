3cd420bcd9f487d8da32aa4082f3546c0bc434c5
Streit-C <114953493+Streit-C@users.noreply.github.com>
2025-08-21 11:17:37 -0400

    Util.py fix

--- _cite/util.py
+++ _cite/util.py
@@ -5,6 +5,8 @@ utility functions for cite process and plugins
 import subprocess
 import json
 import yaml
+import unicodedata
+import re
 from yaml.loader import SafeLoader
 from pathlib import Path
 from datetime import date, datetime
